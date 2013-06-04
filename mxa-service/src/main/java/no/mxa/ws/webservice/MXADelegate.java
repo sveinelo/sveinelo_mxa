6b467a5c638ff6a3f535fd79af6258af7115d4cc
Per Abich <per.abich@gmail.com>
2013-06-04 16:52:29 +0200

    Prodfix for Patentstyret

--- mxa-service/src/main/java/no/mxa/ws/webservice/MXADelegate.java
+++ mxa-service/src/main/java/no/mxa/ws/webservice/MXADelegate.java
@@ -26,12 +26,8 @@ import javax.jws.WebService;
 
 @WebService(targetNamespace = "http://webservice.ws.altut.patent.siriusit.com/", serviceName = "MXA", portName = "MXAPort", wsdlLocation = "no/mxa/ws/xml/wsdl/MXA.wsdl")
 public class MXADelegate {
-	private MXA mxa;
-
 	@Inject
-	public MXADelegate(MXA mxa) {
-		this.mxa = mxa;
-	}
+	private MXA mxa;
 
 	public int submitMessage(String message) {
 		return mxa.submitMessage(message);

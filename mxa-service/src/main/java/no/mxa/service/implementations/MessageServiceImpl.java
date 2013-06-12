0f3fc99b819bfee3badf940f2877ff92b779b620
Svein Erik Løvland <Svein.Erik.Lovland@visma.com>
2013-06-12 12:22:38 +0200

    Fjernet lazy loading av attachment som default i hibernate-konfigurasjonen.

--- mxa-service/src/main/java/no/mxa/service/implementations/MessageServiceImpl.java
+++ mxa-service/src/main/java/no/mxa/service/implementations/MessageServiceImpl.java
@@ -33,6 +33,7 @@ import no.mxa.dto.MessageDTO;
 import no.mxa.service.LogGenerator;
 import no.mxa.service.MessageService;
 
+import org.hibernate.jdbc.TooManyRowsAffectedException;
 import org.springframework.transaction.annotation.Transactional;
 
 public class MessageServiceImpl implements MessageService {
@@ -84,7 +85,7 @@ public class MessageServiceImpl implements MessageService {
 	public MessageDTO searchByMessageKey(String messageKey) {
 		List<MessageDTO> list = this.messageRepository.findByProperty("messageKey", messageKey);
 		if (list.size() != 1) {
-			return null;
+			throw new TooManyRowsAffectedException("For mange rader returnert. Forventet 1 fikk: " + list.size(), 1, list.size());
 		} else {
 			return list.get(0);
 
